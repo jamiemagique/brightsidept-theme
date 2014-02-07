83c414bc1cc0139fb26e4ee6ac8c55a01085d4f0
jamiemagique <jame.eastabrook@gmail.com>
2014-02-07 21:17:59 +0000

    Updated to use jQuery version 1.7.2 via jqmulti module

--- brightsidept/assets/js/brightsidept.js
+++ brightsidept/assets/js/brightsidept.js
@@ -2,12 +2,13 @@
 
 	Drupal.behaviors.brightsidept = function (context) {
 
-		/*
+		// Call mmenu plugin and as ID to the mmenu for the open/close trigger
 		$('.nav-main').clone().attr('id', 'menu').mmenu({
-			
+			classes: 'mm-light',
+			header: true,
+			position: 'right'
 		});
-		*/
 		
 	}
 
-}(jQuery));
+}(jq172));

50e16230c49ac8b445adcd492c4306a3ca90723f
jamiemagique <jame.eastabrook@gmail.com>
2014-02-07 19:26:08 +0000

    Added background gradient and box shadow to brand boxes

--- brightsidept/page.tpl.php
+++ brightsidept/page.tpl.php
@@ -79,7 +79,7 @@
  * @see template_preprocess_page()
  */
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
 
 <head>
@@ -91,7 +91,7 @@
 </head>
 
 <body class="<?php print $body_classes; ?>">
-
+	
 	<div class="l-site">
 	
 		<header class="site-header" role="banner">
@@ -146,6 +146,8 @@
 		<?php print $closure; ?>
 	
 	</div><!-- /.l-site -->
+	
+	<div class="bg-grad"></div>
 
 </body>
 

f002d392380ce1ff221f0ce8ae4d8c96a95bdba8
jamiemagique <jame.eastabrook@gmail.com>
2014-02-07 21:51:44 +0000

    Removed nav-main class incase slide menu is triggered at large screen sizes so styling remains correct

--- brightsidept/assets/js/brightsidept.js
+++ brightsidept/assets/js/brightsidept.js
@@ -3,7 +3,7 @@
 	Drupal.behaviors.brightsidept = function (context) {
 
 		// Call mmenu plugin and as ID to the mmenu for the open/close trigger
-		$('.nav-main').clone().attr('id', 'menu').mmenu({
+		$('.nav-main').clone().removeClass('nav-main').attr('id', 'menu').mmenu({
 			classes: 'mm-light',
 			header: true,
 			position: 'right'
